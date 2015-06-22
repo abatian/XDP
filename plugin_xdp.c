da3b692b0b37d65624f3d5dcdecebe12571a4366
U-Stephane-THINK\user <lapenkov.a@yandex.ru>
2015-06-22 18:13:18 +0300

    dlya_sashki

--- plugin_xdp.c
+++ plugin_xdp.c
@@ -3,8 +3,13 @@
 #include <epan/packet.h>
 
 static int proto_xdp = -1;
-static int hf_xdp_pdu_type = -1;
 static gint ett_xdp = -1;
+static int hf_xdp_pkt_size = -1;
+static int hf_xdp_delivery_flag = -1;
+static int hf_xdp_number_msgs = -1;
+static int hf_xdp_seq_num = -1;
+static int hf_xdp_send_time = -1;
+static int hf_xdp_send_time_ns = -1;
 
 void proto_register_xdp(void)
 {
@@ -14,28 +19,70 @@ void proto_register_xdp(void)
 			"XDP",
 			"xdp"
 		);
+
+	static hf_register_info hf[] = {
+        { &hf_xdp_pkt_size,
+            { "PktSize", "xdp.hdr.pktsize",
+            FT_UINT8, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_xdp_delivery_flag,
+            { "DeliveryFlag", "xdp.hdr.deliveryflag",
+            FT_UINT8, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_xdp_number_msgs,
+            { "NumberMsgs", "xdp.hdr.numbermsgs",
+            FT_UINT8, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_xdp_seq_num,
+            { "SeqNum", "xdp.hdr.seqnum",
+            FT_UINT8, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_xdp_send_time,
+            { "SendTime", "xdp.hdr.sendtime",
+            FT_UINT8, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
+        { &hf_xdp_send_time_ns,
+            { "SendTimeNS", "xdp.hdr.sendtimens",
+            FT_UINT8, BASE_DEC,
+            NULL, 0x0,
+            NULL, HFILL }
+        }
+    };
+
+    static gint *ett[] = { &ett_xdp };
+
+    proto_register_field_array(proto_xdp, hf, array_length(hf));
+    proto_register_subtree_array(ett, array_length(ett));
 }
 
 static void dissect_xdp( tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree )
 {
     col_set_str( pinfo->cinfo, COL_PROTOCOL, "XDP" );
-    //col_clear( pinfo->cinfo, COL_INFO );
-
-    gint offset = 0;
+    col_clear( pinfo->cinfo, COL_INFO );
 
     if(tree)
     {
     	proto_item *ti = NULL;
-    	//proto_tree *xdp_tree = NULL;
+    	proto_tree *xdp_tree = NULL;
 
     	ti = proto_tree_add_item(tree, proto_xdp, tvb, 0, -1, ENC_NA);
-    	//xdp_tree = proto_item_add_subtree(ti, ett_xdp);
-    	//proto_tree_add_item(xdp_tree, hf_xdp_pdu_type, tvb, offset, 2, ENC_LITTLE_ENDIAN);
-    	//xdp_tree = proto_tree_add_item(tree, proto_xdp, tvb, 0, -1, ENC_NA);
-    	//ti = proto_item_add_subtree(xdp_tree, ett_xdp);
-
-    	offset += 2;
-
+    	xdp_tree = proto_item_add_subtree(ti, ett_xdp);
+    	proto_tree_add_item(xdp_tree, hf_xdp_pkt_size, tvb, 0, 2, ENC_LITTLE_ENDIAN);
+    	proto_tree_add_item(xdp_tree, hf_xdp_delivery_flag, tvb, 2, 1, ENC_LITTLE_ENDIAN);
+    	proto_tree_add_item(xdp_tree, hf_xdp_number_msgs, tvb, 3, 1, ENC_LITTLE_ENDIAN);
+    	proto_tree_add_item(xdp_tree, hf_xdp_seq_num, tvb, 4, 4, ENC_LITTLE_ENDIAN);
+    	proto_tree_add_item(xdp_tree, hf_xdp_send_time, tvb, 8, 4, ENC_LITTLE_ENDIAN);
+    	proto_tree_add_item(xdp_tree, hf_xdp_send_time_ns, tvb, 12, 4, ENC_LITTLE_ENDIAN);
     }
 }
 
